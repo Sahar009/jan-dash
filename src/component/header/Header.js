8d51d1c1724fcdfdfcaa8c0672dca91afe003079
Akinwumi sehinde <102046091+Sahar009@users.noreply.github.com>
2024-06-05 10:41:13 +0100

    utils update

--- src/component/header/Header.js
+++ src/component/header/Header.js
@@ -1,18 +1,40 @@
-import React from 'react'
+import React,{useState} from 'react'
+import Loader from '../loader/Loader'
+import { shortenText } from '../../utils/utility'
 
 
 
 const Header = () => {
 
+
+ 
+const [loading, setLoading] = useState(false)
+
+// const handleLoad = () =>{
+//   //post request
+//   fetch('')
+//   .then(() =>{
+//     setLoading(true)
+
+//   })
+//   .then(() =>{
+
+//     setLoading(false)
+//   })
+  
+//   console.log('clicked');
+
+// }
   
   return (
     <div className="--pad header">
     <div className="--flex-between">
+      {loading ? <Loader/> : null}
       <h3>
         <span className="--fw-thin">Welcome, </span>
         <span className="--color-danger">user</span>
       </h3>
-      <button className="--btn --btn-danger" >
+      <button className="--btn --btn-danger">
         Logout
       </button>
     </div>
--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
@@ -12,6 +12,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
 {/* <Provider store={store}> */}
+{}
     <App />
     {/* </Provider> */}
   </React.StrictMode>
