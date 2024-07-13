2f7e6fd578908656b5b457b06c092c78362ccb64
beliskoni <161761767+beliskoni@users.noreply.github.com>
2024-07-13 12:06:29 +0100

    modification on addstudent

--- src/pages/dashboard/addstudent/AddStudent.js
+++ src/pages/dashboard/addstudent/AddStudent.js
@@ -9,8 +9,6 @@ function AddStudent() {
         <section>
         <div className="card">
                 <h1>Add New Student</h1>
-                {/* Photo: <input className=" StudentPhoto" type="text" name="image"
-            value={student.image || ""} onChange={}></input><br/> */}
             <div className="inputfield ">
               <p>First name:</p> <input type="text" placeholder='Enter your Firstname'
                ></input><br/>
@@ -23,13 +21,18 @@ function AddStudent() {
               ></input><br/>
             </div>
             <div className="inputfield ">
+            <p>Date Of Birth:</p> <input className=" dob field" type="date" name="dob"
+              value></input><br/>
+            </div>
+        
+            <div className="inputfield ">
               <p>Age:</p> <input className=" age field" type="number" placeholder="Enter your age" maxlength="2" pattern="^[0-9]{2}$"
               required title="Enter numbers only"></input><br/>
             </div>
             <div class="inputfield">
         <label for="gender"><p>Gender:</p> </label>
-        <select id="gender" name="gender">
-            <option value="" disabled selected>Select gender</option>
+        <select className="options" >
+            <option value="">Select gender</option>
             <option value="male">Male</option>
             <option value="female">Female</option>
             <option value="other">Other</option>
@@ -37,28 +40,21 @@ function AddStudent() {
         </div>
             
             <div className="inputfield ">
-            <label for="gender"><p>Class:</p> </label>
-        <select id="gender" name="gender">
-            <option value="" disabled selected>Select class</option>
-            <option value="male">JSS1</option>
-            <option value="female">JSS2</option>
-            <option value="other">JSS3</option>
-            <option value="other">SSS1</option>
-            <option value="other">SSS2</option>
-            <option value="other">SSS3</option>
-
+            <label for="class"><p>Class:</p> </label>
+        <select className="options">
+            <option value="" >Select class</option>
+            <option value="">JSS1</option>
+            <option value="">JSS2</option>
+            <option value="">JSS3</option>
+            <option value="">SSS1</option>
+            <option value="">SSS2</option>
+            <option value="">SSS3</option>
         </select>
             </div>
-            <div className="inputfield ">
-            <p>Date Of Birth:</p> <input className=" dob field" type="date" name="dob"
-              value></input><br/>
-            </div>
-        
-
-           
-           
-            <button className="btn" onClick={""}>Add New Student</button>
+            <div className='section-2'></div>
+           <button className="btn" onClick={""}>Add New Student</button>
         </div>
+        
             </section>
     );
 }
