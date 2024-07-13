2fe8be745e84ed8916083aab12493528c11bafdf
beliskoni <161761767+beliskoni@users.noreply.github.com>
2024-07-13 01:28:51 +0100

    new styling

--- src/pages/dashboard/addstudent/AddStudent.js
+++ src/pages/dashboard/addstudent/AddStudent.js
@@ -1,82 +1,64 @@
-import { useState } from "react";
+
 import './addstudent.css'
 
 
-function AddStudent(props) {
-    const [student, setStudent] = useState({
-
-            //studentInfo
-            refNo: 0,
-            firstname:'',
-            lastname: '',
-            age: 0,
-            dob: '',
-            gender: 'male',
-            image: '',
-            class: '',
-            address:''
-
-        },
-        {
-            //parentInfo
-            firstname:'',
-            lastName: '',
-            gender: 'male',
-            phonenumber: ''
-           
-        }
-    );
-
-    const addStudentHandler = (e) => {
-        var addStudent = {...student};
-        addStudent[e.target.name] = e.target.value;
-        setStudent(addStudent);
-    }
+function AddStudent() {
 
 
     return(
         <section>
-        <div className="container">
+        <div className="card">
                 <h1>Add New Student</h1>
                 {/* Photo: <input className=" StudentPhoto" type="text" name="image"
-            value={student.image || ""} onChange={addStudentHandler}></input><br/> */}
-            <div className="content">
-              First name: <input className=" firstName field"type="text" name="firstname"
-              value={student.firstname || ""} onChange={addStudentHandler}></input><br/>
+            value={student.image || ""} onChange={}></input><br/> */}
+            <div className="inputfield ">
+              <p>First name:</p> <input type="text" placeholder='Enter your Firstname'
+               ></input><br/>
             </div>
-            <div className="content">
-              Last name: <input className=" lastName field" type="text" name="lastname"
-              value={student.lastname || ""} onChange={addStudentHandler}></input><br/>
+            <div className="inputfield ">
+             <p>Last name:</p>  <input type="text" placeholder='Enter your Lastname'></input><br/>
             </div>
-            <div className="content">
-              Address:  <input className=" Address field" type="text" placeholder="Enter Address" name="address"
-              value={student.address || ""} onChange={addStudentHandler}></input><br/>
+            <div className="inputfield ">
+              <p>Address:</p>  <input type="text" placeholder="Enter Address" name="address"
+              ></input><br/>
             </div>
-            <div className="content">
-              Age: <input className=" age field" type="number" name="age" placeholder="Enter your age" maxlength="2" pattern="^[0-9]{2}$"
-              required title="Enter numbers only"
-            value={student.age || ""} onChange={addStudentHandler}></input><br/>
+            <div className="inputfield ">
+              <p>Age:</p> <input className=" age field" type="number" placeholder="Enter your age" maxlength="2" pattern="^[0-9]{2}$"
+              required title="Enter numbers only"></input><br/>
             </div>
-            <div className="content">
-              Male: <input className="genderM field" type="radio" id="male" name="gender"
-              value="male" onChange={addStudentHandler}></input><span> </span>
-              Female: <input className="genderF field" type="radio" id="female" name="gender"
-              value="female" onChange={addStudentHandler}></input><br/>
+            <div class="inputfield">
+        <label for="gender"><p>Gender:</p> </label>
+        <select id="gender" name="gender">
+            <option value="" disabled selected>Select gender</option>
+            <option value="male">Male</option>
+            <option value="female">Female</option>
+            <option value="other">Other</option>
+        </select>
+        </div>
+            
+            <div className="inputfield ">
+            <label for="gender"><p>Class:</p> </label>
+        <select id="gender" name="gender">
+            <option value="" disabled selected>Select class</option>
+            <option value="male">JSS1</option>
+            <option value="female">JSS2</option>
+            <option value="other">JSS3</option>
+            <option value="other">SSS1</option>
+            <option value="other">SSS2</option>
+            <option value="other">SSS3</option>
+
+        </select>
             </div>
-            <div className="content">
-              Dob:  <input className=" dob field" type="date" name="dob"
-              value={student.dob || ""} onChange={addStudentHandler}></input><br/>
-            </div>
-            <div className="content">
-            Class:  <input className=" class field" type="text" name="class" 
-            value={student.class || ""} onChange={addStudentHandler}></input><br/>
+            <div className="inputfield ">
+            <p>Date Of Birth:</p> <input className=" dob field" type="date" name="dob"
+              value></input><br/>
             </div>
         
 
            
            
-            <button className="btn" onClick={() => props.addStudent(student)}>Add</button>
-            </div>
+            <button className="btn" onClick={""}>Add New Student</button>
+        </div>
             </section>
     );
 }
